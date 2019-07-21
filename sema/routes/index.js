da445b04cbf5554bd6f006f91df3c6143687336b
Adriano Gomes <adrianogsss@gmail.com>
2019-07-21 16:25:56 -0300

    feat: todas routes centralizadas

--- sema/routes/index.js
+++ sema/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const cargoController = require('../controllers').cargo;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+router.get('/api/cargos', cargoController.list);
 
 module.exports = router;
