40dcd1dd79bd607ad53df1f783106b14e1905a67
Adriano Gomes <adrianogsss@gmail.com>
2019-07-21 17:06:01 -0300

    feat: funcoes CRUD para setor

--- sema/controllers/setor.js
+++ sema/controllers/setor.js
@@ -7,4 +7,63 @@ module.exports = {
       .then((setores) => res.status(200).send(setores))
       .catch((error) => { res.status(400).send(error); });
   },
+
+  getById(req, res) {
+    return Setor
+      .findByPk(req.params.id)
+      .then((setor) => {
+        if (!setor) {
+          return res.status(404).send({
+            message: 'Setor não encontrado',
+          });
+        }
+        return res.status(200).send(setor);
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  add(req, res) {
+    return Setor
+      .create({
+        setor: req.body.setor,
+      })
+      .then((setor) => res.status(201).send(setor))
+      .catch((error) => res.status(400).send(error));
+  },
+
+  update(req, res) {
+    return Setor
+      .findByPk(req.params.id)
+      .then(setor => {
+        if (!setor) {
+          return res.status(404).send({
+            message: 'Setor não encontrado!',
+          });
+        }
+        return setor
+          .update({
+            setor: req.body.setor
+          })
+          .then(() => res.status(200).send(setor))
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
+
+  delete(req, res) {
+    return Setor
+      .findByPk(req.params.id)
+      .then(setor => {
+        if (!setor) {
+          return res.status(400).send({
+            message: 'Setor não encontrado',
+          });
+        }
+        return setor
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch((error) => res.status(400).send(error));
+      })
+      .catch((error) => res.status(400).send(error));
+  },
 }
