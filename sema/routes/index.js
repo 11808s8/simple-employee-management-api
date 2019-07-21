08e04d09ceab62b7bd31ef496a298ed3c1aece68
Adriano Gomes <adrianogsss@gmail.com>
2019-07-21 17:06:23 -0300

    feat: rotas para CRUD setor e rotas Nivel

--- sema/routes/index.js
+++ sema/routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const cargoController = require('../controllers').cargo;
 const setorController = require('../controllers').setor;
-
+const nivelController = require('../controllers').nivel;
 /* GET home page. */
 // router.get('/', function(req, res, next) {
 //   res.render('index', { title: 'Express' });
@@ -16,5 +16,16 @@ router.delete('/api/cargo/:id', cargoController.delete);
 
 
 router.get('/api/setores', setorController.list);
+router.get('/api/setor/:id', setorController.getById);
+router.put('/api/setor/:id', setorController.update);
+router.post('/api/setor', setorController.add);
+router.delete('/api/setor/:id', setorController.delete);
+
+router.get('/api/niveis', nivelController.list);
+router.get('/api/nivel/:id', nivelController.getById);
+router.put('/api/nivel/:id', nivelController.update);
+router.post('/api/nivel', nivelController.add);
+router.delete('/api/nivel/:id', nivelController.delete);
+
 
 module.exports = router;
