bcd468d6f1d31818bb4e8c66edc985b3f64cba8e
@11808s8 <adrianogsss@gmail.com>
2019-07-22 18:37:33 -0300

    feat: export do controlador de usuário

--- sema/controllers/index.js
+++ sema/controllers/index.js
@@ -1,9 +1,11 @@
 const cargo = require('./cargo');
 const setor = require('./setor');
 const nivel = require('./nivel');
+const usuario = require('./usuario');
 
 module.exports = {
     cargo,
     setor,
-    nivel
+    nivel,
+    usuario
 };
